f595dbedb714bacd29708505978515fdafe2b85f
Santiago Pocón <spoconb@miumg.edu.gt>
2025-04-08 23:13:23 -0600

    Inicia estructura base del proyecto en spring boot

---
+++ src/main/java/umg/programacion/Beneficio_Cafe/BeneficioCafeApplication.java
@@ -0,0 +1,13 @@
+package umg.programacion.Beneficio_Cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficioCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficioCafeApplication.class, args);
+	}
+
+}
